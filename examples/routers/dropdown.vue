3781eb39a45c323513ab1804f27e5ab277bb69dd
deyu <1579883916@qq.com>
2024-01-18 18:21:14 +0800

    dy 增加测试案例

--- examples/routers/dropdown.vue
+++ examples/routers/dropdown.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
-        <DatePicker :capture="false" size="small" v-model="updatedate" type="datetimerange" placement="bottom-end" placeholder="请输入变更时间" style="width: 150px"></DatePicker>
+        <DatePicker :capture="false" size="small" v-model="updatedate" type="datetimerange" placement="bottom-end"
+                    placeholder="请输入变更时间" style="width: 150px"></DatePicker>
         <Dropdown trigger="click" transfer @on-click="aaaa(111)">
-            <Button  size="small" type="primary">
+            <Button size="small" type="primary">
                 更多
-                <Icon type="md-arrow-dropdown" />
+                <Icon type="md-arrow-dropdown"/>
             </Button>
             <DropdownMenu slot="list">
                 <DropdownItem>驴打滚</DropdownItem>
@@ -31,99 +32,171 @@
         <Poptip trigger="click" transfer title="Title" content="content">
             <Button>Hover</Button>
         </Poptip>
-        <ColorPicker v-model="color1" transfer />
+        <ColorPicker v-model="color1" transfer/>
+        <Dropdown>
+            <a href="javascript:void(0)">
+                北京小吃
+                <Icon type="ios-arrow-down"></Icon>
+            </a>
+            <DropdownMenu slot="list">
+                <DropdownItem>驴打滚</DropdownItem>
+                <DropdownItem>炸酱面</DropdownItem>
+                <DropdownItem>豆汁儿</DropdownItem>
+                <Dropdown placement="right-start">
+                    <DropdownItem>
+                        北京烤鸭
+                        <Icon type="ios-arrow-forward"></Icon>
+                    </DropdownItem>
+                    <DropdownMenu slot="list">
+                        <DropdownItem>挂炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                        <DropdownItem>焖炉烤鸭</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+                <DropdownItem>冰糖葫芦</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown>
+            <a href="javascript:void(0)">
+                北京小吃
+                <Icon type="ios-arrow-down"></Icon>
+            </a>
+            <template #list>
+                <DropdownMenu>
+                    <DropdownItem>驴打滚</DropdownItem>
+                    <DropdownItem>炸酱面</DropdownItem>
+                    <DropdownItem>豆汁儿</DropdownItem>
+                    <Dropdown placement="left">
+                        <DropdownItem>
+                            北京烤鸭
+                            <Icon type="ios-arrow-forward"></Icon>
+                        </DropdownItem>
+                        <template #list>
+                            <DropdownMenu>
+                                <DropdownItem>挂炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                                <DropdownItem>焖炉烤鸭</DropdownItem>
+                            </DropdownMenu>
+                        </template>
+                    </Dropdown>
+                    <DropdownItem>冰糖葫芦</DropdownItem>
+                </DropdownMenu>
+            </template>
+        </Dropdown>
     </div>
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                updatedate:null,
-                color1: '#19be6b',
-                value1: [],
-                data: [{
-                    value: 'beijing',
-                    label: '北京',
-                    children: [
-                        {
-                            value: 'gugong',
-                            label: '故宫'
-                        },
-                        {
-                            value: 'tiantan',
-                            label: '天坛'
-                        },
-                        {
-                            value: 'wangfujing',
-                            label: '王府井'
-                        }
-                    ]
-                }, {
-                    value: 'jiangsu',
-                    label: '江苏',
-                    children: [
-                        {
-                            value: 'nanjing',
-                            label: '南京',
-                            children: [
-                                {
-                                    value: 'fuzimiao',
-                                    label: '夫子庙',
-                                }
-                            ]
-                        },
-                        {
-                            value: 'suzhou',
-                            label: '苏州',
-                            children: [
-                                {
-                                    value: 'zhuozhengyuan',
-                                    label: '拙政园',
-                                },
-                                {
-                                    value: 'shizilin',
-                                    label: '狮子林',
-                                }
-                            ]
-                        }
-                    ],
-                }],
-                cityList: [
+export default {
+    data() {
+        return {
+            updatedate: null,
+            color1: '#19be6b',
+            value1: [],
+            data: [{
+                value: 'beijing',
+                label: '北京',
+                children: [
                     {
-                        value: 'New York',
-                        label: 'New York'
+                        value: 'gugong',
+                        label: '故宫'
                     },
                     {
-                        value: 'London',
-                        label: 'London'
+                        value: 'tiantan',
+                        label: '天坛'
                     },
                     {
-                        value: 'Sydney',
-                        label: 'Sydney'
+                        value: 'wangfujing',
+                        label: '王府井'
+                    }
+                ]
+            }, {
+                value: 'jiangsu',
+                label: '江苏',
+                children: [
+                    {
+                        value: 'nanjing',
+                        label: '南京',
+                        children: [
+                            {
+                                value: 'fuzimiao',
+                                label: '夫子庙',
+                            }
+                        ]
                     },
                     {
-                        value: 'Ottawa',
-                        label: 'Ottawa'
-                    },
-                    {
-                        value: 'Paris',
-                        label: 'Paris'
-                    },
-                    {
-                        value: 'Canberra',
-                        label: 'Canberra'
+                        value: 'suzhou',
+                        label: '苏州',
+                        children: [
+                            {
+                                value: 'zhuozhengyuan',
+                                label: '拙政园',
+                            },
+                            {
+                                value: 'shizilin',
+                                label: '狮子林',
+                            }
+                        ]
                     }
                 ],
-                model1: []
-            }
-        },
-        methods: {
-            aaaa(){
-                this.$Message.info("aaaaa")
-            }
-        },
-        mounted () {
-
+            }],
+            cityList: [
+                {
+                    value: 'New York',
+                    label: 'New York'
+                },
+                {
+                    value: 'London',
+                    label: 'London'
+                },
+                {
+                    value: 'Sydney',
+                    label: 'Sydney'
+                },
+                {
+                    value: 'Ottawa',
+                    label: 'Ottawa'
+                },
+                {
+                    value: 'Paris',
+                    label: 'Paris'
+                },
+                {
+                    value: 'Canberra',
+                    label: 'Canberra'
+                }
+            ],
+            model1: []
+        };
+    },
+    methods: {
+        aaaa() {
+            this.$Message.info('aaaaa');
         }
+    },
+    mounted() {
+
     }
+};
 </script>
