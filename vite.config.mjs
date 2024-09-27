9108d0a02cb30eaf526a412734f9298981222c4d
deyu <1579883916@qq.com>
2024-09-27 15:16:38 +0800

    build(builder): 边缘问题处理

--- vite.config.mjs
+++ vite.config.mjs
@@ -33,9 +33,7 @@ export default defineConfig(({mode}) => {
             },
         },
         define: {
-            'process.env': JSON.stringify({
-                VERSION: pkg.version,
-            }),
+            'process.env.VERSION': JSON.stringify(pkg.version),
         }
     };
 });
